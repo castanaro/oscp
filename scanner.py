763bea185bfab3005b85f70cf48f2af624481ec7
Michael Castanaro <66240320+castanaro@users.noreply.github.com>
2021-06-29 14:42:15 -0700

    Update scanner.py
    Functional updates.

--- scanner.py
+++ scanner.py
@@ -1,16 +1,26 @@
 #!/bin/python3
 
+
+'''
+Thanks to TCM's Heath Adams for his Practical Ethical Hacking Course.
+TCM's PNPT course can be found here: https://certifications.tcm-sec.com/product/practical-network-penetration-tester-with-training/
+'''
+
 import sys
 import socket
 from datetime import datetime
 import time
 
 #Define our target
-if len(sys.argv) == 2:
+if len(sys.argv) == 4:
         target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+        port1 = sys.argv[2]
+        port2 = sys.argv[3]
 else:
-        print("You need 2 arguments for this script ex.: python3 scanner.py <ip>")
+        print("You need arguments for this script ex.: python3 scanner.py <ip> <port 1> <port 2> to create port range")
 
+port1 = int(port1) #cast string to int for the for loop later on
+port2 = int(port2) #same thing here
 
 #add a banner
 
@@ -21,7 +31,7 @@ print("Time started: " + str(datetime.now()))
 print("-" * 50)
 
 try:
-        for port in range(50,55):
+        for port in range(port1,port2):
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 socket.setdefaulttimeout(1)
                 result = s.connect_ex((target,port)) #returns an error indicator
@@ -44,8 +54,8 @@ except socket.error:
         sys.exit()
 
 
-print("*" * 50)
+print("-" * 50)
 timeend = time.time()
 print("Time ended: " + str(datetime.now()))
 print("Total Time: " + str(float(timeend - timestart)) + " seconds.")
-print("*" * 50)
+print("-" * 50)
